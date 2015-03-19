fab53c3589d617bfb840628053b62492e30332f9
kfzx-zhenggb <zhenggb@sdc.icbc.com.cn>
2015-03-19 18:53:31 +0800

    增加一个案例：两本不同的书

--- potter/potter20150319_1/src/potter20150319_1/Potter.java
+++ potter/potter20150319_1/src/potter20150319_1/Potter.java
@@ -2,12 +2,15 @@ package potter20150319_1;
 
 public class Potter {
 
+	private static final double discout2 = 0.95;
 	double singlePrice = 8.0;
 
 	public double total(int... booklist) {
 
 		if (booklist.length == 1) {
 			return singlePrice;
+		}else if(booklist.length==2 && booklist[0]!=booklist[1]){
+			return singlePrice*2*discout2;
 		}else{
 			int samebookcount=0;
 			for(int i=0;i<booklist.length;i++)
--- potter/potter20150319_1/src/potter20150319_1/testPotter.java
+++ potter/potter20150319_1/src/potter20150319_1/testPotter.java
@@ -6,25 +6,31 @@ import org.junit.Test;
 
 public class testPotter {
 
+	private static Potter POTTER = new Potter();
 	@Test
 	public void test空购物清单() {
 		
-		assertBookTotal(0.0, new Potter().total());
+		assertBookTotal(0.0, POTTER.total());
 	}
 	@Test
 	public void test只有一本书() {
 		
-		assertBookTotal(8.0, new Potter().total(0));
+		assertBookTotal(8.0, POTTER.total(0));
 	}
 	@Test
 	public void test两本相同书() {
 		
-		assertBookTotal(8.0*2, new Potter().total(0,0));
+		assertBookTotal(8.0*2, POTTER.total(0,0));
 	}
 	@Test
 	public void test三本相同书() {
 		
-		assertBookTotal(8.0*3, new Potter().total(0,0,0));
+		assertBookTotal(8.0*3, POTTER.total(0,0,0));
+	}
+	@Test
+	public void test两本不同书() {
+		
+		assertBookTotal(8.0*2*0.95, POTTER.total(0,1));
 	}
 	private void assertBookTotal(double expect, double atual) {
 		assertEquals(expect,atual,0.001);
