83846df96c237c2a6d22724d39be254263af6d74
kfzx-zhenggb <zhenggb@sdc.icbc.com.cn>
2015-03-19 18:41:31 +0800

    增加一个案例：两本相同的书。

--- potter/potter20150319_1/src/potter20150319_1/Potter.java
+++ potter/potter20150319_1/src/potter20150319_1/Potter.java
@@ -5,6 +5,11 @@ public class Potter {
 	double singlePrice = 8.0;
 
 	public double total(int... booklist) {
+
+		if (booklist.length == 2 && booklist[0] == booklist[1]) {
+			return singlePrice * 2;
+		}
+
 		if (booklist.length == 1) {
 			return singlePrice;
 		}
--- potter/potter20150319_1/src/potter20150319_1/testPotter.java
+++ potter/potter20150319_1/src/potter20150319_1/testPotter.java
@@ -16,6 +16,11 @@ public class testPotter {
 		
 		assertBookTotal(8.0, new Potter().total(0));
 	}
+	@Test
+	public void test两本相同书() {
+		
+		assertBookTotal(8.0*2, new Potter().total(0,0));
+	}
 
 	private void assertBookTotal(double expect, double atual) {
 		assertEquals(expect,atual,0.001);
